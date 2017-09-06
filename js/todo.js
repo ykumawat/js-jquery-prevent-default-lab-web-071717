495bbbe67961dd15712c5120584ff0bf122299e4
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-09-05 21:55:06 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   // call functions here
+  submitForm()
 });
 
 // define functions here
+
+function submitForm() {
+  $('form').on('submit', function(event) {
+    let task = $('#item').val()
+    let list = document.getElementById('list')
+    list.innerHTML += '<li>' + task + '</li>'
+    event.preventDefault()
+  })
+}
